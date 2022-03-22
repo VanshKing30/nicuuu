c50cb508641c44d41d7715e8429362e6683ff752
Vansh <vanshwaldeo360@gmail.com>
2022-03-22 21:42:03 +0530

    Added negative

---
+++ negative.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = -4 ;
+
+    if(n<0){
+        cout << "Negative number" <<endl;
+    }
+    else{
+        cout<<"It is Poitive" << endl;
+    }
+
+}
